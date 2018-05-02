8eda4b22794ee2d727d146745808dfe9bbdb2bec
cristimitran <cristimitran@gmail.com>
2018-05-02 23:57:01 +0300

    counting duplicates

--- Counting Duplicates.js
+++ Counting Duplicates.js
@@ -1,24 +1,63 @@
-let text = "aabbcde"
+let text = "Indivisibilities"
 
 let lowercase = text.toLowerCase()
     let length = lowercase.length //?
-    let array = lowercase.split('') //?
-    let count = 0
-  
+    let array1 = lowercase.split('') //?
+  let tempArray = array1
+  let result = []
 
+//compare array to itself
 function duplicateCount(text){
-    let k = 0
-    let temparray = array.slice(1)//?
-    for(i=0;i<length;i++){
-      if(array[i]/*?*/ === temparray[i]/*?*/) {
-
+   for(i=0; i<length; i++) {
+    console.log(text[i]) 
+    for(j=i + 1; j<length; j++){
+      console.log(text[j]) 
+      if(text[i] == text[j]) {
+        result.push(text[i])
       }
-        
+   }
     }
   }
 
-duplicateCount(text)
+   
 
-/* 
-str.toLowerCase()
-*/
+duplicateCount(lowercase)
+
+let counts = {}
+for (i = 0; i < length; i++) {
+  counts[lowercase[i]] = 1 + (counts[lowercase[i]] || 0);
+}
+
+//count unique items in an object
+function countUnique(iterable) {
+  return new Set(iterable).size;
+}
+
+console.log(countUnique(result))
+
+console.log(result)
+console.log(counts)
+
+// function duplicateCount(text) {
+//   let lowercase = text.toLowerCase()
+//   let length = lowercase.length
+//   let result = []
+
+//   for (i = 0; i < length; i++) {
+//       for (j = i + 1; j < length; j++) {
+//           if (lowercase[i] == lowercase[j]) {
+//               result.push(lowercase[i])
+//           }
+//       }
+//   }
+
+//   function countUnique(iterable) {
+//       return new Set(iterable).size;
+//   }
+
+//  return countUnique(result)
+// }
+
+// function duplicateCount(text){
+//   return (text.toLowerCase().split('').sort().join('').match(/([^])\1+/g) || []).length;
+// }
