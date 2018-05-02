6b6ecfeffffca88a0d2a8a3a08a198b95d239a93
Cristi Mitran <cristimitran@gmail.com>
2018-05-02 17:56:47 +0300

    whyyyy

--- Consecutive strings.js
+++ Consecutive strings.js
@@ -7,35 +7,54 @@
 // }
 
 let arr = ["zone", "abigail", "theta", "form", "libe", "zas"]
+//let arr = ["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"]
+//let arr = ["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"]
 let newArr = []
 let n = arr.length
 let k = 2
 let result = []
 
-
-console.log(arr[arr.length - k-5])
-
-for (i=0; i<n; i++) {
-    newArr.push(arr.slice(i, k+i))
+function longestConsec(strarr, k) {
+    var longest = "";
+    for(var i=0;k>0 && i<=strarr.length-k;i++){
+        console.log(strarr.length-k)
+      var tempArray = strarr.slice(i,i+k); //?
+      var tempStr = tempArray.join(""); //?
+      if(tempStr.length > longest.length){
+        longest = tempStr; //?
+      }
+    }
+    return longest;
 }
 
-let difference = (newArr.length - k)
+console.log(longestConsec(arr, 2))
 
-console.log(newArr)
+// console.log(arr[arr.length - k-5])
 
-console.log(newArr[difference])
+// for (i=0; i<n; i++) {
+//     newArr.push(arr.slice(i, k+i))
+// }
 
-for (i=0; i<newArr.length-1; i++) {
-    console.log(newArr[difference-i])
-    for(j=1; j<newArr.length-1; j++) {
-        console.log(newArr[difference-j])
-        
-          
+// let difference = (newArr.length - k)
+
+// console.log(newArr)
+
+// console.log(newArr[difference])
+
+// for (i=0; i<newArr.length-(k-1); i++) {
+//     console.log(newArr[difference-i])
+//     console.log((newArr[difference-i].join()).length)
+//     for(j=1; j<newArr.length-(k-1); j++) {
+//         console.log(newArr[difference-j])
+//         console.log((newArr[difference-j].join()).length)
+//        if((newArr[difference-j].join()).length > (newArr[difference-i].join()).length) {
+//         result = newArr[difference-j]
+//         }
     
-    }
-} 
+//     }
+// } 
 
-console.log(result) //?
+// console.log(result) //?
 
 // if((newArr[difference-j][i] + newArr[difference-j][j]).length > (newArr[difference-i][i] + newArr[difference-i][j]).length) {
 //     result.push(newArr[difference-j][i] + newArr[difference-j][j])
---
+++ Counting Duplicates.js
@@ -0,0 +1,24 @@
+let text = "aabbcde"
+
+let lowercase = text.toLowerCase()
+    let length = lowercase.length //?
+    let array = lowercase.split('') //?
+    let count = 0
+  
+
+function duplicateCount(text){
+    let k = 0
+    let temparray = array.slice(1)//?
+    for(i=0;i<length;i++){
+      if(array[i]/*?*/ === temparray[i]/*?*/) {
+
+      }
+        
+    }
+  }
+
+duplicateCount(text)
+
+/* 
+str.toLowerCase()
+*/
